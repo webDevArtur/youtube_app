b4be119984f83a1afbfe9356372aa50e526df82e
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2025-05-28 19:22:43 +0300

    Update videoStore.ts

--- src/store/videoStore.ts
+++ src/store/videoStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import { useVideosFetcher } from '../hooks/useVideosFetcher'
 
 function getVideoId(video: any): string {
